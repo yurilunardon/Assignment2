f50e0a521298b206189afd14c43f36179cd06fa2
Yuri Lunardon <lunardonyuri@icloud.com>
2024-05-15 23:27:06 +0200

    Primi 3 numeri
    Close #5

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -1,8 +1,25 @@
+////////////////////////////////////////////////////////////////////
+// Yuri Lunardon 2079244
+// Andrea Perozzo 2082849
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.mtss;
 
+import java.lang.StringBuilder;
+
 public class IntegerToRoman {
-    public static String convert(int number){
-        // TODO
-        return null;
-    }
-}
+    public static String convert(int number) {
+        StringBuilder romanNumber = new StringBuilder();
+
+        if (number > 0) {
+            while(number > 0) {
+            romanNumber.append("I");
+            number--;
+            }
+        
+        return romanNumber.toString();
+        }
+
+    return "";
+   }
+ }
---
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////////////
+// Yuri Lunardon 2079244
+// Andrea Perozzo 2082849
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+ import java.util.stream.Stream;
+
+ import org.junit.jupiter.params.ParameterizedTest;
+ import org.junit.jupiter.params.provider.Arguments;
+ import org.junit.jupiter.params.provider.MethodSource;
+
+ import static org.junit.jupiter.api.Assertions.assertEquals;
+ import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+ public class IntegerToRomanTest {
+
+   @ParameterizedTest
+   @MethodSource("numberToRoman")
+   void checkNumbers(int number, String romanNum) {
+     assertEquals(romanNum, IntegerToRoman.convert(number));
+   }
+
+   private static Stream<Arguments> numberToRoman() {
+     return Stream.of(
+         arguments(1, "I"),
+         arguments(2, "II"),
+         arguments(3, "III")
+         );
+   } 
+ }
