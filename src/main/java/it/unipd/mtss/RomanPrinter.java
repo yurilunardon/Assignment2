cb0654f54f9505970d2cab8e1b50aa6f7138bb3a
andrea-perozzo <andrea.perozzo.2@studenti.unipd.it>
2024-05-16 12:06:31 +0200

    Implementazione classe RomanPrinter
    Close #14

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -7,10 +7,59 @@ package it.unipd.mtss;
 
   public class RomanPrinter {
       public static String print(int num){
-          return printAsciiArt(IntegerToRoman.convert(num));
-      }
-      private static String printAsciiArt(String romanNumber){
-          //TODO
-          return null;
-      }
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+
+    private static String printAsciiArt(String romanNumber){
+
+        if(romanNumber.isEmpty()) {return null;}
+
+        String[] splitted = romanNumber.split("");
+
+        for (int i = 0; i<romanNumber.length(); i++) {
+            switch(splitted[i]) {
+            case "I":
+                splitted[i] = " ___ \n|_ _|\n | | \n | | \n|___|\n";
+                break;
+
+            case "V":
+                splitted[i] = "__     __\n\\ \\   / /\n"+
+                    " \\ \\ / / \n  \\ V /  \n   \\_/   \n";
+                break;
+
+            case "X":
+                splitted[i] = "__  __\n\\ \\/ /\n \\  / \n /  \\ \n/_/\\_\\\n";
+                break;
+
+            case "L":
+                splitted[i] = " _     \n| |    \n| |    \n| |___ \n|_____|\n";
+                break;
+
+            case "C":
+                splitted[i] = "  ____ \n / ___|\n| |    \n| |___ \n \\____|\n";
+                break;
+
+            case "D":
+                splitted[i] = " ____  \n|  _ \\ \n| | | |\n| |_| |\n|____/ \n";
+                break;
+
+            case "M":
+                splitted[i] =
+                    " __  __ \n|  \\/  |\n| |\\/| |\n| |  | |\n|_|  |_|\n";
+                break;
+            }
+        }
+
+        for (int j=0; j<5; j++){
+            for (int i = 0; i<romanNumber.length(); i++) {
+                String temp = splitted[i].split("\n")[0];
+                System.out.print(temp);
+
+                splitted[i] = splitted[i].substring(temp.length()+1);
+            }
+            System.out.print("\n");
+        }
+
+        return null;
+    }
 }
